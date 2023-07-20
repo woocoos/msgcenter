cf27de1bc101aa7bf4b0ffcdbee5528f6ec55b11
xhyu <xhyu@qeelyn.com>
2023-07-20 10:47:25 +0800

    菜单右侧点击弹出新窗口

--- web/src/components/Layout/index.tsx
+++ web/src/components/Layout/index.tsx
@@ -19,6 +19,7 @@ import zhCN from 'antd/locale/zh_CN';
 import enUS from 'antd/locale/en_US';
 import { Locale } from 'antd/es/locale';
 import { ConfigProvider } from 'antd';
+import { EnterOutlined, NodeExpandOutlined } from '@ant-design/icons';
 
 export default () => {
   const [userState, userDispatcher] = store.useModel('user'),
@@ -114,7 +115,12 @@ export default () => {
           />,
           <DarkMode />,
         ]}
-        menuItemRender={(item, defaultDom) => (item.path ? <Link to={item.path}>{defaultDom}</Link> : defaultDom)}
+        menuItemRender={(item, defaultDom) => (item.path ? <>
+          <Link to={item.path}>{defaultDom}</Link>
+          <NodeExpandOutlined className={styles.menuIconPopup} onClick={() => {
+            window.open(item.path)
+          }} />
+        </> : defaultDom)}
       >
         <AliveScope>
           <Outlet />
