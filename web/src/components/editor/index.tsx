d50024c07d427f5d8205842697ba61cb134e3c3b
xhyu <512106269@qq.com>
2025-05-16 10:04:05 +0800

    文本编辑器的cdn读取改为环境变量设置

--- web/src/components/editor/index.tsx
+++ web/src/components/editor/index.tsx
@@ -1,5 +1,9 @@
 import Editor, { loader } from '@monaco-editor/react';
 
-loader.config({ paths: { vs: 'https://files.qeelyn.com/cdn/monaco-editor/0.52.2/min/vs' } })
+const ICE_MONACO_CDN_HOST = process.env.ICE_MONACO_CDN_HOST
+
+if (ICE_MONACO_CDN_HOST) {
+  loader.config({ paths: { vs: ICE_MONACO_CDN_HOST } })
+}
 
 export default Editor;
